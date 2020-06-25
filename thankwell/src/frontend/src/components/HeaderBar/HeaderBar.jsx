0a53b50b71436a24b2dbfad3b42c147c5fe2df2e
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2020-06-26 00:08:20 +0100

    Add logo to header

--- thankwell/src/frontend/src/components/HeaderBar/HeaderBar.jsx
+++ thankwell/src/frontend/src/components/HeaderBar/HeaderBar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Bell } from 'react-feather';
-import { User } from 'react-feather';
-import { Link } from 'react-router-dom';
-
+import {Bell, User} from 'react-feather';
+import {Link} from 'react-router-dom';
+import logo from "../../assets/logo.png";
 import "./HeaderBar.scss";
 
 const HeaderBar = () => (
     <div className="HeaderBar">
-        <Link className="HeaderBar-link" to="/">Home</Link>
-        <Link className="HeaderBar-link" to="/give-thanks">Give Thanks</Link>
-        <Link className="HeaderBar-link" to="/groups">Groups</Link>
-        <Link className="HeaderBar-link" to="/messages">Messages</Link>
-        <div className="HeaderBar-icon"><Bell/></div>
-        <div className="HeaderBar-icon"><User/></div>
+        <div className="d-flex flex-row justify-content-between w-100">
+            <Link to="/" className="logo-link"><img src={logo} alt="logo" className="logo"/></Link>
+            <div className="d-flex links">
+                <Link className="HeaderBar-link" to="/">Home</Link>
+                <Link className="HeaderBar-link" to="/give-thanks">Give Thanks</Link>
+                <Link className="HeaderBar-link" to="/groups">Groups</Link>
+                <Link className="HeaderBar-link" to="/messages">Messages</Link>
+                <div className="HeaderBar-icon"><Bell/></div>
+                <div className="HeaderBar-icon"><User/></div>
+            </div>
+        </div>
     </div>
 );
 
