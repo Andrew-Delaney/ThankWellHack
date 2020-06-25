56101174447991a65338e138ce78521713b5b093
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2020-06-25 16:10:33 +0100

    Hook in controller advice

--- thankwell/src/main/java/uk/co/blackpepper/thankwell/rest/ControllerAdvice.java
+++ thankwell/src/main/java/uk/co/blackpepper/thankwell/rest/ControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
