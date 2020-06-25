ea144e02aaa998af80d60fd761360bb3ce144ecb
Andrew Delaney <andrew.delaney@blackpepper.co.uk>
2020-06-26 00:57:21 +0100

    add further styling to thank you card widgets

--- thankwell/src/frontend/src/components/Dashboard/Dashboard.jsx
+++ thankwell/src/frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import ThankYouCard from '../ThankYouCard/ThankYouCard';
+import thanksImg from "../../assets/thanks.jpg";
 
 const Dashboard = () => {
     const [allThanks, setAllThanks] = useState([]);
@@ -12,12 +13,12 @@ const Dashboard = () => {
     }, []);
 
     return (
-        <div>
+        <div className="Dashboard">
             <h2 className="m-3 pb-4 font-weight-bolder">Home</h2>
             {allThanks && allThanks.map((thanks, index) =>
              <div key={index}>
                 <a href={`thanks/${thanks.id}`}>
-                    <ThankYouCard recipient={thanks.recipient} message={thanks.message} image="../assets/thanks.jpg"/>
+                    <ThankYouCard recipient={thanks.recipient} message={thanks.message} image={thanksImg}/>
                 </a>
                 <br/>
             </div>)}
--- thankwell/src/frontend/src/components/ThankYouCard/ThankYouCard.jsx
+++ thankwell/src/frontend/src/components/ThankYouCard/ThankYouCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import "./ThankYouCard.scss";
+
 var ThankYouCard = ({ image, message, recipient }) => (
     <div className="ThankYouCard">
-        <image className="ThankYouCard-image" src={image}></image>
-        <div className="ThankYouCard-recipient">{recipient}</div>
-        <div className="ThankYouCard-message">{message}</div>
+        <img className="ThankYouCard-image" src={image} alt="Thank you!"></img>
+        <div className="ThankYouCard-text">
+            <h5 className="ThankYouCard-text-recipient">{recipient}</h5>
+            <div className="ThankYouCard-text-message">{message}</div>
+        </div>
     </div>
 );
 
