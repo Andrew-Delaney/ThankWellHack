8b9f3a2bb9d48b59c51f87ca58179ebbe6924f84
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2020-06-25 13:38:16 +0100

    Add bootstrap springboot

---
+++ thankwell/src/main/java/uk/co/blackpepper/thankwell/ThankwellApplication.java
@@ -0,0 +1,13 @@
+package uk.co.blackpepper.thankwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThankwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThankwellApplication.class, args);
+	}
+
+}
---
+++ thankwell/src/test/java/uk/co/blackpepper/thankwell/ThankwellApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.blackpepper.thankwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThankwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
