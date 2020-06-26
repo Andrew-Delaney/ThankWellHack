cbd1fc71c3338f15b460288ba1ec033072a609cf
Andrew Delaney <andrew.delaney@blackpepper.co.uk>
2020-06-26 10:43:10 +0100

    change images on groups

--- thankwell/src/frontend/src/components/MyGroups/MyGroups.jsx
+++ thankwell/src/frontend/src/components/MyGroups/MyGroups.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import GroupCard from '../GroupCard/GroupCard';
 import Group from "../../assets/group.jpg";
+import Birtellis from "../../assets/birtellis.jpg";
+import Hospital from "../../assets/qe.jpg";
+import BlackPepper from "../../assets/bp.jpg";
 
 import "./MyGroups.scss";
 
@@ -8,10 +11,10 @@ var MyGroups = () => (
     <div className="MyGroups mt-4">
         <h2 className="MyGroups-heading m-3 pb-4 font-weight-bolder">My Groups</h2>
         <div className="MyGroups-row row no-gutters ml-4">
-            <GroupCard image={Group} name="Black Pepper"/>
+            <GroupCard image={BlackPepper} name="Black Pepper"/>
             <GroupCard image={Group} name="ThankWell"/>
-            <GroupCard image={Group} name="Birtellis"/>
-            <GroupCard image={Group} name="Queen Elizabeth Hospital"/>
+            <GroupCard image={Birtellis} name="Birtellis"/>
+            <GroupCard image={Hospital} name="Queen Elizabeth Hospital"/>
         </div>
     </div>
 );
