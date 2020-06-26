ed7cba3b02f47f086e25ca8ec5d378de38c6e36a
Andrew Delaney <andrew.delaney@blackpepper.co.uk>
2020-06-26 01:41:34 +0100

    display cards using flex

--- thankwell/src/frontend/src/components/Dashboard/Dashboard.jsx
+++ thankwell/src/frontend/src/components/Dashboard/Dashboard.jsx
@@ -2,26 +2,28 @@ import React, {useEffect, useState} from 'react';
 import ThankYouCard from '../ThankYouCard/ThankYouCard';
 import thanksImg from "../../assets/thanks.jpg";
 
+import "./Dashboard.scss";
+
 const Dashboard = () => {
     const [allThanks, setAllThanks] = useState([]);
 
     useEffect( () => {
         fetch(`/api/all`)
             .then(response => response.json())
-            .then(data => setAllThanks(data))
-            .then(data => console.log(data));
+            .then(data => setAllThanks(data));
     }, []);
 
     return (
         <div className="Dashboard">
-            <h2 className="m-3 pb-4 font-weight-bolder">Home</h2>
-            {allThanks && allThanks.map((thanks, index) =>
-             <div key={index}>
-                <a href={`thanks/${thanks.id}`}>
-                    <ThankYouCard recipient={thanks.recipient} message={thanks.message} image={thanksImg}/>
-                </a>
-                <br/>
-            </div>)}
+            <h2 className="mt-3 mb-3 pb-4 font-weight-bolder">Home</h2>
+            <div className="Dashboard-cards row no-gutters">
+                {
+                    allThanks && allThanks.map((thanks, index) => {
+                        console.log(thanks);
+                        return <ThankYouCard key={index} recipient={thanks.recipient} message={thanks.message} image={thanks.image ? thanks.image : thanksImg} id={thanks.id}/>
+                    })
+                }
+            </div>
         </div>
     );
 };
--- thankwell/src/frontend/src/components/ThankYouCard/ThankYouCard.jsx
+++ thankwell/src/frontend/src/components/ThankYouCard/ThankYouCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import "./ThankYouCard.scss";
+import { Link } from 'react-router-dom';
 
-var ThankYouCard = ({ image, message, recipient }) => (
-    <div className="ThankYouCard">
-        <img className="ThankYouCard-image" src={image} alt="Thank you!"></img>
-        <div className="ThankYouCard-text">
-            <h5 className="ThankYouCard-text-recipient">{recipient}</h5>
-            <div className="ThankYouCard-text-message">{message}</div>
-        </div>
+var ThankYouCard = ({ image, message, recipient, id }) => (
+    <div className="ThankYouCard col-10 col-sm-8 col-md-4 col-lg-3">
+        <Link to={`/thanks/${id}`}>
+            <img className="ThankYouCard-image" src={image} alt="Thank you!"></img>
+            <div className="ThankYouCard-text">
+                <h5 className="ThankYouCard-text-recipient">{recipient}</h5>
+                <div className="ThankYouCard-text-message">{message}</div>
+            </div>
+        </Link>
     </div>
 );
 
